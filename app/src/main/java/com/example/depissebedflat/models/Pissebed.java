d87e005b3ffa5eaf14d6caee00a43be382a1e8cf
Renske <renske@fabrique.nl>
2021-06-11 12:19:48 +0200

    add some logic

--- app/src/main/java/com/example/depissebedflat/models/Pissebed.java
+++ app/src/main/java/com/example/depissebedflat/models/Pissebed.java
@@ -1,5 +1,7 @@
 package com.example.depissebedflat.models;
 
+import java.util.ArrayList;
+
 /*
     A class depicting a plant of a certain type and size, and the color of the pot it is in.
 */
@@ -9,6 +11,13 @@ public class Pissebed {
     private String species;
     private String color;
     private int size;
+
+    // Determine what items are liked by this pissebed
+    // Construct happiness bar based on amount of liked items
+    // There is also a required item you need before they come at all!
+
+    private ArrayList<Item> likedItems;
+    private Item requiredItem;
 
     public Pissebed(String name, String species, String color, int size) {
         this.name = name;
@@ -21,31 +30,35 @@ public class Pissebed {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getSpecies() {
         return species;
-    }
-
-    public void setSpecies(String species) {
-        this.species = species;
     }
 
     public String getColor() {
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
     public int getSize() {
         return size;
     }
 
-    public void setSize(int size) {
-        this.size = size;
+    public ArrayList<Item> getLikedItems() {
+        return likedItems;
+    }
+
+    public Item getRequiredItem() {
+        return requiredItem;
+    }
+
+    public Boolean likesItem(Item item) {
+        return this.likedItems.contains(item);
+    }
+
+    public Boolean requirementsMet(ArrayList<Item> itemsPresent) {
+        if (this.requiredItem != null) {
+            return itemsPresent.contains(this.requiredItem);
+        }
+        // We require nothing so requirement is met
+        return true;
     }
 }
