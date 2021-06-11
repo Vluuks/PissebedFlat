0921d3804ce441edb15867441a10d9c7acc5a256
Renske <renske@fabrique.nl>
2021-06-11 14:00:09 +0200

    generate items

--- app/src/main/java/com/example/depissebedflat/models/Item.java
+++ app/src/main/java/com/example/depissebedflat/models/Item.java
@@ -1,5 +1,10 @@
 package com.example.depissebedflat.models;
 
+import com.example.depissebedflat.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Item {
 
     private String name;
@@ -14,6 +19,17 @@ public class Item {
         this.drawableId = drawableId;
         this.price = price;
         this.owned = owned;
+    }
+
+    public static ArrayList<Item> getAllItems() {
+        ArrayList<Item> items = new ArrayList<>(Arrays.asList(
+                // Drawable id here is the drawable for listview, not main view
+                new Item("Bank", "Handig als je ergens lekker wil chillen", R.drawable.bank, 1000, true),
+                new Item("Kleed", "Voor pissebedden die graag op de grond zitten", R.drawable.bank, 200, true),
+                new Item("Plant", "Ook handig om onder te schuilen", R.drawable.bank, 80, false),
+                new Item("Voetbal", "Sommige pissebedden zijn dol op voetbal", R.drawable.bank, 50, false)
+            ));
+        return items;
     }
 
     public String getName() {
