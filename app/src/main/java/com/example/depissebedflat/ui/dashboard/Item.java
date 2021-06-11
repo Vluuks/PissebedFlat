16129698b423d319365d5dda1f7c8ce376ea7e2c
Renske <renske@fabrique.nl>
2021-06-11 12:04:39 +0200

    item class

--- app/src/main/java/com/example/depissebedflat/ui/dashboard/Item.java
+++ app/src/main/java/com/example/depissebedflat/ui/dashboard/Item.java
@@ -1,4 +1,52 @@
 package com.example.depissebedflat.ui.dashboard;
 
+import com.example.depissebedflat.models.Pissebed;
+
+import java.util.ArrayList;
+
 class Item {
+
+    private String name;
+    private String description;
+    private int drawableId;
+    private int price;
+
+    public Item(String name, String description, int drawableId, int price) {
+        this.name = name;
+        this.description = description;
+        this.drawableId = drawableId;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDrawableId() {
+        return drawableId;
+    }
+
+    public void setDrawableId(int drawableId) {
+        this.drawableId = drawableId;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
